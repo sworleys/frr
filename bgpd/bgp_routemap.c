f11a9d8320836fbce83a32522956ca61f7cb389c
Nitin Soni <nsoni@cumulusnetworks.com>
2018-12-20 20:20:08 -0800

    bgpd: route-map fails to filter type-5 routes
    Route-map filtering is based on the value of
    "bgp->adv_cmd_rmap[afi][safi].map". For example, we advertise routes in
    bgp_evpn_advertise_type5_routes() based on the value of
    "bgp->adv_cmd_rmap[afi][safi].map". This variable gets populated in vty
    handler bgp_evpn_advertise_type5. This variable will not get populated
    if we have not yet applied the route-map configuration. The fix is to
    correctly populate "bgp->adv_cmd_rmap[afi][safi].map" in
    bgp_route_map_process_update() if it has not been populated before.
    
    Ticket: CM-23263
    Signed-off-by: Nitin Soni <nsoni@cumulusnetworks.com>
    Reviewed-by: CCR-8163

--- bgpd/bgp_routemap.c
+++ bgpd/bgp_routemap.c
@@ -3104,12 +3104,16 @@ static void bgp_route_map_process_update(struct bgp *bgp, const char *rmap_name,
 	FOREACH_AFI_SAFI (afi, safi) {
 		if (bgp->adv_cmd_rmap[afi][safi].name &&
 		    strcmp(rmap_name, bgp->adv_cmd_rmap[afi][safi].name) == 0) {
+			/* Make sure the route-map is populated here if not already done */
+			bgp->adv_cmd_rmap[afi][safi].map = map;
 			if (BGP_DEBUG(zebra, ZEBRA))
 				zlog_debug(
 					   "Processing route_map %s update on advertise type5 route command",
 					   rmap_name);
-			bgp_evpn_withdraw_type5_routes(bgp, afi, safi);
-			bgp_evpn_advertise_type5_routes(bgp, afi, safi);
+			if (route_update) {
+				bgp_evpn_withdraw_type5_routes(bgp, afi, safi);
+				bgp_evpn_advertise_type5_routes(bgp, afi, safi);
+			}
 		}
 	}
 }
