4a79a7a25f24fd0ebe7a776e6589737ab01325ca
Donald Sharp <sharpd@cumulusnetworks.com>
2019-01-25 20:11:21 -0500

    zebra_rib: Add code to create/remove nexthop groups

--- zebra/zebra_rib.c
+++ zebra/zebra_rib.c
@@ -55,6 +55,7 @@
 #include "zebra/zebra_vxlan.h"
 #include "zebra/zapi_msg.h"
 #include "zebra/zebra_dplane.h"
+#include "zebra/zebra_nhg.h"
 
 /*
  * Event, list, and mutex for delivery of dataplane results
@@ -2522,6 +2523,7 @@ static void rib_addnode(struct route_node *rn,
 void rib_unlink(struct route_node *rn, struct route_entry *re)
 {
 	rib_dest_t *dest;
+	rib_table_info_t *info;
 
 	assert(rn && re);
 
@@ -2543,7 +2545,11 @@ void rib_unlink(struct route_node *rn, struct route_entry *re)
 	if (dest->selected_fib == re)
 		dest->selected_fib = NULL;
 
+	info = srcdest_rnode_table_info(rn);
+	zebra_nhg_release(info->afi, re);
+
 	nexthops_free(re->ng.nexthop);
+
 	XFREE(MTYPE_RE, re);
 }
 
@@ -2773,6 +2779,7 @@ int rib_add_multipath(afi_t afi, safi_t safi, struct prefix *p,
 	if (src_p)
 		apply_mask_ipv6(src_p);
 
+	zebra_nhg_find(afi, &re->ng, re);
 	/* Set default distance by route type. */
 	if (re->distance == 0) {
 		re->distance = route_distance(re->type);
