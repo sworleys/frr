e9e21ed611e1b50abfe9145b27a1493a09280a47
Chirag Shah <chirag@cumulusnetworks.com>
2019-02-08 20:42:34 -0800

    bgpd: advertise svi ip as macip zebra parse api
    Ticket:CM-23782
    
    Signed-off-by: Chirag Shah <chirag@cumulusnetworks.com>

--- bgpd/bgp_zebra.h
+++ bgpd/bgp_zebra.h
@@ -61,6 +61,8 @@ extern struct interface *if_lookup_by_ipv6_exact(struct in6_addr *, ifindex_t,
 						 vrf_id_t);
 extern int bgp_zebra_advertise_subnet(struct bgp *bgp, int advertise,
 				      vni_t vni);
+extern int bgp_zebra_advertise_svi_macip(struct bgp *bgp, int advertise,
+					 vni_t vni);
 extern int bgp_zebra_advertise_gw_macip(struct bgp *, int, vni_t);
 extern int bgp_zebra_advertise_all_vni(struct bgp *, int);
 extern int bgp_zebra_dup_addr_detection(struct bgp *bgp);
