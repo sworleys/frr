271a16c6638217d7fde19ed951a8b0397d8a6ff4
Stephen Worley <sworley@cumulusnetworks.com>
2019-03-08 10:23:34 -0500

    zebra: Set the INSTALLED flags on nexthop entries we receive
    Add SETs to the flags on nexthop entries to mark
     installed/uninstalled from the kernel.
    
    Signed-off-by: Stephen Worley <sworley@cumulusnetworks.com>

--- zebra/rt_netlink.c
+++ zebra/rt_netlink.c
@@ -2242,6 +2242,8 @@ int netlink_nexthop_change(struct nlmsghdr *h, ns_id_t ns_id, int startup)
 				return -1;
 			}
 		}
+		SET_FLAG(nhe->flags, NEXTHOP_GROUP_INSTALLED);
+
 	} else if (h->nlmsg_type == RTM_DELNEXTHOP) {
 		if (!nhe) {
 			flog_warn(
@@ -2250,6 +2252,8 @@ int netlink_nexthop_change(struct nlmsghdr *h, ns_id_t ns_id, int startup)
 				id);
 			return -1;
 		}
+
+		UNSET_FLAG(nhe->flags, NEXTHOP_GROUP_INSTALLED);
 
 		// TODO: Run some active check on all route_entry's?
 		if (nhe->refcnt) {
