76b7001df9ff66f42af894c60bfad80131ae4421
Chirag Shah <chirag@cumulusnetworks.com>
2019-05-24 17:50:46 -0700

    bgpd: fix debug to have proper nhop display
    Display nexthop based on route type.
    
    Ticket:CM-25129
    Reviewed By:CCR-8741
    Testing Done:
    
    evpn route:
    *  [2]:[0]:[0]:[48]:[aa:aa:aa:aa:01:1a]:[32]:[45.0.2.111]
        36.0.0.25              0 64000 5560 i
    
    Old:
    BGP: Tx route add VRF 46 45.0.2.111/32 metric 0 tag 0 flags 0x1409 nhnum 1
    BGP:   nhop [1]: 2400:1d:: if 50 VRF 46
    
    New:
    BGP: import evpn prefix [2]:[aa:aa:aa:aa:01:1a]:[45.0.2.111]/224 as
    ip prefix 45.0.2.111/32 in vrf vrf1
    
    BGP: bgp_zebra_announce: p=45.0.2.111/32, bgp_is_valid_label: 2
    BGP: Tx route add VRF 46 45.0.2.111/32 metric 0 tag 0 flags 0x1409 nhnum 1
    BGP:   nhop [1]: 36.0.0.25 if 50 VRF 46
    
    Signed-off-by: Chirag Shah <chirag@cumulusnetworks.com>

--- bgpd/bgp_evpn.c
+++ bgpd/bgp_evpn.c
@@ -1867,7 +1867,7 @@ static int install_evpn_route_entry_in_vrf(struct bgp *bgp_vrf,
 		ip_prefix_from_type5_prefix(evp, pp);
 
 	if (bgp_debug_zebra(NULL)) {
-		zlog_debug("installing evpn prefix %s as ip prefix %s in vrf %s",
+		zlog_debug("import evpn prefix %s as ip prefix %s in vrf %s",
 			   prefix2str(evp, buf, sizeof(buf)),
 			   prefix2str(pp, buf1, sizeof(buf)),
 			   vrf_id_to_name(bgp_vrf->vrf_id));
