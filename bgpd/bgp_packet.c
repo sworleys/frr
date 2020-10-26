328460ff83bff7fe28a998f4feb2ac3f32f3de90
vivek <vivek@cumulusnetworks.com>
2020-10-25 21:21:55 -0700

    bgpd: Enhance OPEN Tx debug log
    Signed-off-by: Vivek Venkatraman <vivek@nvidia.com>

--- bgpd/bgp_packet.c
+++ bgpd/bgp_packet.c
@@ -565,9 +565,9 @@ void bgp_open_send(struct peer *peer)
 
 	if (bgp_debug_neighbor_events(peer))
 		zlog_debug(
-			"%s sending OPEN, version %d, my as %u, holdtime %d, id %s",
-			peer->host, BGP_VERSION_4, local_as, send_holdtime,
-			inet_ntoa(peer->local_id));
+			"%s fd %d sending OPEN, version %d, my as %u, holdtime %d, id %s",
+			peer->host, peer->fd, BGP_VERSION_4, local_as,
+			send_holdtime, inet_ntoa(peer->local_id));
 
 	/* Dump packet if debug option is set. */
 	/* bgp_packet_dump (s); */
