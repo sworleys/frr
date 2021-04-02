2057368e259b7d638c8d62d0d0b94ffe65494b19
Stephen Worley <sworley@nvidia.com>
2021-04-02 14:38:07 -0400

    Revert "[CUMULUS] zebra: remove proto only NHG in show run"
    This reverts commit 2755330e49558a0f13aaa58b9d4cea5d56ce1f0e.

--- zebra/zebra_vty.c
+++ zebra/zebra_vty.c
@@ -3582,9 +3582,8 @@ static int config_write_protocol(struct vty *vty)
 	if (!zebra_nhg_kernel_nexthops_enabled())
 		vty_out(vty, "no zebra nexthop kernel enable\n");
 
-	/* CUMULUS: Disable seeing this in `show run` since its default */
-	//if (zebra_nhg_proto_nexthops_only())
-	//	vty_out(vty, "zebra nexthop proto only\n");
+	if (zebra_nhg_proto_nexthops_only())
+		vty_out(vty, "zebra nexthop proto only\n");
 
 	return 1;
 }
